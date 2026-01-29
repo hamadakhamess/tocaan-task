460e47f7d948bb7c35a53dda8af8893d6e763f9e
Hamada Khamess <65801995+hamadakhamess@users.noreply.github.com>
2026-01-29 15:49:30 +0200

    update : add new Exceptions when resource not found and post man collection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => false,
+                    'code' => 404,
+                    'message' => $e->getPrevious() instanceof ModelNotFoundException 
+                        ? 'Resource not found' 
+                        : 'Route not found',
+                    'data' => null,
+                ], 404);
+            }
+        });
     })->create();
